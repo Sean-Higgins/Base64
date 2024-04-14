c9b28c8dc26f18a0b64b66f9abace0e1dcfc3fdb
Sean Higgins <seanhiggins639@gmail.com>
2024-04-14 10:17:52 -0700

    First commit of the Rust cargo project and its files.

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+/*
+ * Program: Base64
+ * Summary: A base64 encoding and decoding program written in the Rust language.
+ * Programmer: Sean B. Higgins
+ * Date: April 14, 2024
+ */
+
+fn main() {
+    println!("Hello, world!");
+}
